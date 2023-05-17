a06b3161c530430cc609d1443b03054a1001928f
alzbarbosa <alzbarbosa@gmail.com>
2023-05-16 17:41:42 -0700

    Foruth Version

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # add by me
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
